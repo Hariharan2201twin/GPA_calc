a7fb2ac2d786eee136c2ff580e586b5f50ceee5e
Hariharan2201twin <48782425+Hariharan2201twin@users.noreply.github.com>
2019-06-18 23:57:42 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>c</title>
+</head>
+<body>
+<form action="c1.php" method="post">
+Name:<input type="text" name="name"><br>
+Semester:<input type="text" name="a"><br>
+Year:<input type="text" name="b"><br>
+Dept:<input type="text" name="c"><br>
+<input type="submit" value="login">
+</form>
+</body>
+</html>
